1e2ab0f4a0a446fc58ace3313436510eba2d808b
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-06-23 16:37:54 +0700

    Update logo image source in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ export default function Navbar() {
           <div className="flex items-center gap-2">
             <div className="relative w-8 h-8">
               <Image 
-                src="/images/logo/koje-logo.png"
+                src="/images/logo/koje_16_jun_-removebg-preview.png"
                 alt="KOJE Natural Logo"
                 fill
                 className="object-contain"
