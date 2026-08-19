4f09502ce08420e3e8994a63cdf900422ddcbe05
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-08-19 16:41:53 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 5,
   userScalable: true,
+  viewportFit: 'cover',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#ffffff' },
     { media: '(prefers-color-scheme: dark)', color: '#0A0F1A' },
